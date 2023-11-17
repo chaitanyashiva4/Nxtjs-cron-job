51e298d9fb1f27c5f59b1d2ac19545291dbc891e
chaitanya <chaitanyabatchu4@gmail.com>
2023-11-17 10:24:55 +0530

    Data-for every minute

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 import cron from 'node-cron';
+import axios from 'axios';
 
-const performTask = () => {
-  console.log('Cron job running every minute');
+const fetchData = async () => {
+  try {
+    const response = await axios.get('https://98fa-2401-4900-4fbd-42ab-71e1-5b00-6b3f-f1bf.ngrok-free.app/api/genarateFakeJson');
+    console.log('Received data:', response.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 };
+
 cron.schedule('*/1 * * * *', () => {
-  performTask();
+  console.log("Cron Running");
+  fetchData();
 });
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  return NextResponse.json({ message: 'Cron job started' });
+  return NextResponse.json({ message: 'Scheduled data fetch every minute' });
 }
