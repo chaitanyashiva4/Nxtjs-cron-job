4e15315b9445abb259c390e4a33c5f4a492227e7
chaitanya <chaitanyabatchu4@gmail.com>
2023-11-17 10:27:51 +0530

    changes

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,10 +2,14 @@ import { NextRequest, NextResponse } from 'next/server';
 import cron from 'node-cron';
 import axios from 'axios';
 
+let receivedDataCount = 0;
+
 const fetchData = async () => {
   try {
     const response = await axios.get('https://98fa-2401-4900-4fbd-42ab-71e1-5b00-6b3f-f1bf.ngrok-free.app/api/genarateFakeJson');
+    receivedDataCount += 1;
     console.log('Received data:', response.data);
+    console.log('Total data count:', receivedDataCount);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
